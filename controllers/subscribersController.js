7fd738dc0b0944560106ea79a33a77c8383b9179
pikaboo <stevinjack1ina1000@gmail.com>
2023-01-23 14:31:49 +0630

    first release

--- controllers/subscribersController.js
+++ controllers/subscribersController.js
@@ -1,43 +1,43 @@
 const mongoose = require("mongoose"),
-  Subscriber = require("../models/subscriber");
+    Subscriber = require("../models/subscriber");
 
 exports.getAllSubscribers = (req, res, next) => {
-  Subscriber.find({})
-    .exec()
-    .then((subscribers) => {
-      res
-        .render("subscribers", {
-          subscribers: subscribers,
-        })
-        .catch((error) => {
-          console.log(error.message);
-          return [];
-        })
-        .then(() => {
-          console.log("Promise completed");
+    Subscriber.find({})
+        .exec()
+        .then((subscribers) => {
+            res
+                .render("subscribers", {
+                    subscribers: subscribers,
+                })
+                .catch((error) => {
+                    console.log(error.message);
+                    return [];
+                })
+                .then(() => {
+                    console.log("Promise completed");
+                });
         });
-    });
 };
 
 exports.getSubscriptionPage = (req, res) => {
-  res.render("contact");
+    res.render("contact");
 };
 
 exports.saveSubscriber = (req, res) => {
-  let newSubscriber = new Subscriber({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    zipCode: req.body.zipCode,
-  });
-  newSubscriber
-    .save()
-    .then((result) => {
-      res.render("thanks");
-    })
-    .catch((error) => {
-      if (error) {
-        res.send("error");
-      }
+    let newSubscriber = new Subscriber({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        zipCode: req.body.zipCode,
     });
+    newSubscriber
+        .save()
+        .then((result) => {
+            res.render("thanks");
+        })
+        .catch((error) => {
+            if (error) {
+                res.send("error");
+            }
+        });
 };
--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/confetti_cuisine", {
-  useNewUrlParser: true,
+    useNewUrlParser: true,
 }); // connecting to local database
 
 const db = mongoose.connection; // assign connected database to db variable
 mongoose.Promise = global.Promise;
 db.once("open", () => {
-  console.log("Successfully connected to MongoDB using Mongoose");
+    console.log("Successfully connected to MongoDB using Mongoose");
 }); // display messsage for once when it's done
 
 const express = require("express"),
-  app = express(),
-  layouts = require("express-ejs-layouts"),
-  homeController = require("./controllers/homeController"),
-  errorController = require("./controllers/errorController"),
-  subscribersController = require("./controllers/subscribersController");
+    app = express(),
+    layouts = require("express-ejs-layouts"),
+    homeController = require("./controllers/homeController"),
+    errorController = require("./controllers/errorController"),
+    subscribersController = require("./controllers/subscribersController");
 
 app.set("view engine", "ejs"); // tell the view engine of Express.js to use ejs format
 // set the view engine of Express.js to ejs engine
@@ -23,9 +23,9 @@ app.set("port", process.env.PORT || 3000); // set the port number
 app.use(layouts); // middleware to use ejs layouts
 app.use(express.static("public")); // for static files such as images, css and js
 app.use(
-  express.urlencoded({
-    extended: false,
-  })
+    express.urlencoded({
+        extended: false,
+    })
 ); // middleware to encode incoming url context
 app.use(express.json()); // middleware to encode incoming data to json format
 
@@ -33,11 +33,11 @@ app.get("/", homeController.showCourses);
 app.get("/courses", homeController.showCourses); // home page
 app.get("/contact", subscribersController.getSubscriptionPage); // contact page viewer page
 app.get(
-  "/subscribers",
-  subscribersController.getAllSubscribers,
-  (req, res, next) => {
-    console.log(req.data);
-  }
+    "/subscribers",
+    subscribersController.getAllSubscribers,
+    (req, res, next) => {
+        console.log(req.data);
+    }
 );
 
 app.post("/subscribe", subscribersController.saveSubscriber); // contact form submission
@@ -46,5 +46,5 @@ app.use(errorController.pageNotFoundError); // custom error middleware
 app.use(errorController.internalServerError); // custom error middleware
 
 app.listen(app.get("port"), () => {
-  console.log(`Server is running at http://localhost:${app.get("port")}`);
+    console.log(`Server is running at http://localhost:${app.get("port")}`);
 });
